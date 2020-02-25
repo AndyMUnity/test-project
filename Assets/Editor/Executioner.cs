218b856410ba6276c1da43ee140db1253e58f486
Andy Milsom <andrewmi@unity3d.com>
2020-02-25 17:17:54 +0000

    Playerbuild

--- Assets/Editor/Executioner.cs
+++ Assets/Editor/Executioner.cs
@@ -1,7 +1,112 @@
-using System.Collections;
-using System.Collections.Generic;
+using Boo.Lang;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace ContinuousIntegration
+{
+    public static class BuildPipeline
+    {
+        
+        private class BuildSettings
+        {
+            public bool Development;
+            public BuildSettings()
+            {
+                Development = EditorUserBuildSettings.development;
+            }
+
+            public void SetSettings()
+            {
+                if( EditorUserBuildSettings.development != Development )
+                    EditorUserBuildSettings.development = Development;
+            }
+        }
+
+        private static BuildSettings SetupBuildArguments()
+        {
+            BuildSettings preBuildValues = new BuildSettings();
+            int releaseMode = 0;
+            
+            // check for development / non-development
+            string[] args = System.Environment.GetCommandLineArgs();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if( args[i] == "-development" )
+                    releaseMode = -1;
+                else if( args[i] == "-release" )
+                    releaseMode = 1;
+            }
+
+            if( releaseMode != 0 )
+                EditorUserBuildSettings.development = releaseMode == -1;
+
+            return preBuildValues;
+        }
+
+
+
+
+
+        private enum ExitCode
+        {
+            Success = 0,
+            UnknownError,
+            PlayerBuildCanceled,
+            PlayerBuildUnknownError,
+            PlayerBuildFailed
+        }
+
+        private static ExitCode GetExitCode( BuildResult playerBuildResult )
+        {
+            switch( playerBuildResult )
+            {
+                case BuildResult.Cancelled:
+                    return ExitCode.PlayerBuildCanceled;
+                case BuildResult.Failed:
+                    return ExitCode.PlayerBuildFailed;
+                case BuildResult.Unknown:
+                    return ExitCode.PlayerBuildUnknownError;
+            }
+
+            return ExitCode.Success;
+        }
+
+        private static BuildPlayerOptions InitialOptions()
+        {
+            BuildPlayerOptions options = new BuildPlayerOptions();
+            options.target = EditorUserBuildSettings.activeBuildTarget;
+            options.targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            List<string> scenes = new List<string>();
+            foreach( EditorBuildSettingsScene settingsScene in EditorBuildSettings.scenes )
+            {
+                if( settingsScene.enabled )
+                    scenes.Add( settingsScene.path );
+            }
+            options.scenes = scenes.ToArray();
+            options.options = BuildOptions.None;
+            options.locationPathName = "MyBuild";
+            return options;
+        }
+        
+        public static void BuildPlayer()
+        {
+            ExitCode code = ExitCode.UnknownError;
+            BuildPlayerOptions options = InitialOptions();
+            
+            string[] args = System.Environment.GetCommandLineArgs();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if( args[i] == "-development" )
+                    options.options |= BuildOptions.Development;
+            }
+
+            BuildReport r = UnityEditor.BuildPipeline.BuildPlayer( options );
+            code = GetExitCode( r.summary.result );
+            EditorApplication.Exit( (int)code );
+        }
+    }
+}
 
 public static class Executioner
 {
