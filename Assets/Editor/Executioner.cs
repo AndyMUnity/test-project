72fb9b73fc8795811af3547b11fac861ab6eb668
Andy Milsom <andrewmi@unity3d.com>
2020-02-26 17:41:20 +0000

    more direct commands

--- Assets/Editor/Executioner.cs
+++ Assets/Editor/Executioner.cs
@@ -1,62 +1,23 @@
-using Boo.Lang;
-using UnityEngine;
+using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 
 namespace ContinuousIntegration
 {
+    internal enum ExitCode
+    {
+        Success = 0,
+        UnknownError,
+        PlayerBuildCanceled,
+        PlayerBuildUnknownError,
+        PlayerBuildFailed
+    }
+    
     public static class BuildPipeline
     {
+        private static string[] args;
         
-        private class BuildSettings
-        {
-            public bool Development;
-            public BuildSettings()
-            {
-                Development = EditorUserBuildSettings.development;
-            }
-
-            public void SetSettings()
-            {
-                if( EditorUserBuildSettings.development != Development )
-                    EditorUserBuildSettings.development = Development;
-            }
-        }
-
-        private static BuildSettings SetupBuildArguments()
-        {
-            BuildSettings preBuildValues = new BuildSettings();
-            int releaseMode = 0;
-            
-            // check for development / non-development
-            string[] args = System.Environment.GetCommandLineArgs();
-            for (int i = 0; i < args.Length; i++)
-            {
-                if( args[i] == "-development" )
-                    releaseMode = -1;
-                else if( args[i] == "-release" )
-                    releaseMode = 1;
-            }
-
-            if( releaseMode != 0 )
-                EditorUserBuildSettings.development = releaseMode == -1;
-
-            return preBuildValues;
-        }
-
-
-
-
-
-        private enum ExitCode
-        {
-            Success = 0,
-            UnknownError,
-            PlayerBuildCanceled,
-            PlayerBuildUnknownError,
-            PlayerBuildFailed
-        }
-
         private static ExitCode GetExitCode( BuildResult playerBuildResult )
         {
             switch( playerBuildResult )
@@ -67,6 +28,10 @@ namespace ContinuousIntegration
                     return ExitCode.PlayerBuildFailed;
                 case BuildResult.Unknown:
                     return ExitCode.PlayerBuildUnknownError;
+                case BuildResult.Succeeded:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException( nameof(playerBuildResult), playerBuildResult, null );
             }
 
             return ExitCode.Success;
@@ -74,52 +39,67 @@ namespace ContinuousIntegration
 
         private static BuildPlayerOptions InitialOptions()
         {
-            BuildPlayerOptions options = new BuildPlayerOptions();
-            options.target = EditorUserBuildSettings.activeBuildTarget;
-            options.targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
             List<string> scenes = new List<string>();
             foreach( EditorBuildSettingsScene settingsScene in EditorBuildSettings.scenes )
             {
                 if( settingsScene.enabled )
                     scenes.Add( settingsScene.path );
             }
-            options.scenes = scenes.ToArray();
-            options.options = BuildOptions.None;
-            options.locationPathName = "MyBuild";
-            return options;
+            
+            DateTime now = DateTime.Now;
+            string outputPath = "Builds/" + EditorUserBuildSettings.activeBuildTarget + "/" +
+                                       now.Year + "-" + now.Month + "-" + now.Day + 
+                                       "_" + now.TimeOfDay.TotalSeconds;
+            
+            return new BuildPlayerOptions
+            {
+                target = EditorUserBuildSettings.activeBuildTarget,
+                targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
+                scenes = scenes.ToArray(),
+                options = BuildOptions.None,
+                locationPathName = outputPath
+            };
+        }
+
+        public static void Run()
+        {
+            args = System.Environment.GetCommandLineArgs();
+            
+            for (int i = 0; i < args.Length; i++)
+            {
+                if( args[i] == "-buildPlayer" )
+                    BuildPlayer();
+                else if( args[i] == "-buildAssetBundles" )
+                    BuildBundles();
+            }
         }
         
-        public static void BuildPlayer()
+        private static void BuildPlayer()
         {
             ExitCode code = ExitCode.UnknownError;
+
             BuildPlayerOptions options = InitialOptions();
-            
-            string[] args = System.Environment.GetCommandLineArgs();
             for (int i = 0; i < args.Length; i++)
             {
                 if( args[i] == "-development" )
                     options.options |= BuildOptions.Development;
+                else if( args[i] == "-buildPath" )
+                {
+                    ++i;
+                    options.locationPathName = args[i];
+                }
             }
 
             BuildReport r = UnityEditor.BuildPipeline.BuildPlayer( options );
             code = GetExitCode( r.summary.result );
             EditorApplication.Exit( (int)code );
         }
+        
+        private static void BuildBundles()
+        {
+            ExitCode code = ExitCode.UnknownError;
+            EditorApplication.Exit( (int)code );
+        }
     }
 }
 
-public static class Executioner
-{
-    public static void ExitWithCodeThree()
-    {
-        string[] args = System.Environment.GetCommandLineArgs();
-         for (int i = 0; i < args.Length; i++)
-         {
-             Debug.Log ("ARG " + i + ": " + args [i]);
-             if (args [i] == "-error") {
-                 EditorApplication.Exit( 3 );
-             }
-         }
-        EditorApplication.Exit( 0 );
-    }
-}
